e1fd6e9d401917c96d70669d2abbc77cd216dfcc
Ra <ravicity999@gmail.com>
2024-04-20 05:31:28 -0400

    chore: fixed a responsiveness bug and apastrophe linting error

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -6,7 +6,7 @@ const login = () => {
 			<div className="grid grid-cols-1 lg:grid-cols-2 gap-10 max-w-7xl w-full">
 					
 				<div className="flex flex-col gap-10 items-left justify-start px-8 py-[14vh] lg:px-16 h-screen">
-					<span className="text-black font-bold text-2xl md:text-3xl sm:text-center">Access Your Account</span>
+					<span className="text-black font-bold text-2xl md:text-3xl w-full sm:w-2/3 sm:text-center">Access Your Account</span>
 
 					<div className="rounded-lg border-2 border-solid border-gray-200 w-full lg:w-2/3">
 						<div className="flex flex-row items-center justify-center gap-4 py-2 px-10">
@@ -41,7 +41,7 @@ const login = () => {
 					<div className="bg-black rounded-lg border-2 border-solid border-gray-200 h-12 flex flex-row items-center justify-center w-full lg:w-2/3">
 						<Link href={'/login'}><span className="text-white text-center">Sign in</span></Link>
 					</div>
-					<span className="text-black text-sm font-semibold text-center w-full lg:w-2/3">Don't have an account? Sign up</span>
+					<span className="text-black text-sm font-semibold text-center w-full lg:w-2/3">Don&apos;t have an account? Sign up</span>
 				</div>
 				<div className="hidden lg:block">
 				<Image src={'/images/food.jpg'} alt="food" width={1000} height={1000} />
